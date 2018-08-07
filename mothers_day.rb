a9f967c40a9d116a38e24c7b44711251285c1d77
salehlayaly <lsaleh@hcstonline.org>
2018-08-07 17:00:25 +0000

    automatically backed up by learn

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,7 +1,7 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
 def mothers_day(name = "Mom")
-  return "Happy Mother's day #{name}"
+  return "I hope you're having a great mother's day #{name}"
 end 
-
+mothers_day("Beyonce")
   
 
